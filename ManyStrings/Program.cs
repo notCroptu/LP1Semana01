4b1c2b6badb438f60f2223a928a7c9ea484d78db
notCroptu <martinsmariana1972@gmail.com>
2024-02-21 22:02:33 +0000

    Variaveis string Concatenadas e interpoladas Ex5

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlTypes;
 
 namespace ManyStrings
 {
@@ -7,8 +8,6 @@ namespace ManyStrings
         static void Main(string[] args)
         {
             //Exercise 4
-
-            Console.WriteLine("Hello, World!");
 
             string s1 = "Linha \n nova.";
             string s2 = "Tab \t .";
@@ -19,6 +18,21 @@ namespace ManyStrings
             Console.WriteLine(s2);
             Console.WriteLine(s3);
             Console.WriteLine(s4);
+
+            //Exercise 5
+
+            sbyte bite = 18;
+            sbyte x = 3;
+            sbyte y = 2;
+            string add = $"{x} plus {y} is {x + y}.";
+            string whatbite = $"I can bite you {bite + 5} times.";
+            string concatenated = "u" + x + 10;
+            string concatenated2 = "This many bites: " + bite;
+
+            Console.WriteLine(add);
+            Console.WriteLine(whatbite);
+            Console.WriteLine(concatenated);
+            Console.WriteLine(concatenated2);
 
         }
     }
