52ff431f4b23a9e55b42ddb17d91e70d167faf7c
notCroptu <martinsmariana1972@gmail.com>
2024-02-21 22:39:33 +0000

    Programa Ex7

---
+++ AskForInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduza um número inteiro.");
+            int numint = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduza um número real.");
+            float numfloat = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A soma de {numint} e {numfloat} é {numint + numfloat}.");
+        }
+    }
+}
