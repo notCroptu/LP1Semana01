fcd7252daa5fea0a66bbe96ac57705809fea2e98
notCroptu <martinsmariana1972@gmail.com>
2024-02-21 19:43:23 +0000

    Exercicio 2

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Cria o programa
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Programa imprime duas strings
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
