4c7e264a03372d01813368c0e5de8d50d0942822
notCroptu <martinsmariana1972@gmail.com>
2024-02-23 13:38:44 +0000

    Centra Pela Virgula ExExtra

---
+++ CentraPelaVirgula/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace CentraPelaVirgula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Introduza o primeiro número real.");
+            float realum = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduza o segundo número real.");
+            float realdois = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduza o terceiro número real.");
+            float realtres = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduza o quarto número real.");
+            float realquatro = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{realum,6:f2}");
+            Console.WriteLine($"{realdois,6:f2}");
+            Console.WriteLine($"{realtres,6:f2}");
+            Console.WriteLine($"{realquatro,6:f2}");
+        }
+    }
+}
